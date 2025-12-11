f9ffb20c154d7a1b2afb32af83f5e4cfc8f59c04
pablogr28 <pagorom28@gmail.com>
2025-12-12 00:18:19 +0100

    Subo cambios

--- src/main/java/com/e_commerce/security/SecurityConfig.java
+++ src/main/java/com/e_commerce/security/SecurityConfig.java
@@ -106,7 +106,7 @@ public class SecurityConfig {
     @Bean
     CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
-        configuration.setAllowedOrigins(List.of("http://localhost:4200"));
+        configuration.setAllowedOrigins(List.of("http://localhost:4200","https://proyecto-integrado-angular.vercel.app"));
         configuration.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS","PATCH"));
         configuration.setAllowedHeaders(List.of("*"));
         configuration.setAllowCredentials(true);
